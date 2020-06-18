e1d7e8fecbe41d9679a2317d50e8ea9d6e7ce002
Alexander Kleinhans <alex@rowsheet.com>
2020-06-18 10:30:47 -0600

    save.sh

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import json
 with open("dashboard.yaml", 'r') as stream:
     try:
         parsed = yaml.safe_load(stream)
-        print(json.dumps(parsed, indent=2, sort_keys=True))
+        print(json.dumps(parsed, indent=1, sort_keys=False))
     except yaml.YAMLError as exc:
         print(exc)
