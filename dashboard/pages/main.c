8060f33fd7f2c19b18c026f673a074507b3da0d0
Alexander Kleinhans <alex@rowsheet.com>
2020-08-03 11:03:47 -0600

    buffer overflow test

--- dashboard/pages/main.c
+++ dashboard/pages/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("change");
+    printf("starting program...\n");
+    
+    printf("enter input: ");
+    char buff[5];
+    gets(buff);
+    printf("your input was:\n%s", buff);
+    
     return 0;
 }
